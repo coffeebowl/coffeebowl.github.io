c42cb5fea876ce8dcf431597e14f0bd312e87d45
potatopat <patrick.schlecht@gmail.com>
2023-10-26 09:24:51 +0200

    init

---
+++ assets/HelpPage.6e2ab78c.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.38fda81d.js";import{a7 as a,D as r,H as t,I as c,K as o}from"./index.20e62cc8.js";import"./render.358e7716.js";const s={};function f(l,n){return r(),t(e,{class:"flex flex-center"},{default:c(()=>[o(" Hilfe ")]),_:1})}var x=a(s,[["render",f]]);export{x as default};
